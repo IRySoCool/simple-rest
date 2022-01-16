c3d85e60997ace6b8e2a7291676223a058cc800b
IRySoCool <irysocool116@gmail.com>
2022-01-17 01:10:03 +0800

    add comment

--- app/Src/Config.hs
+++ app/Src/Config.hs
@@ -4,9 +4,10 @@ module Src.Config where
 
 import           Options.Generic
 
+-- add the db connection settings here
 data Config w = Config
     { port :: w ::: Int <!> "8080" <?> "Port number for the app"
-    , foo  :: w ::: String <!> "bar" <?> "This field is useless"
+    , foo  :: w ::: String <!> "bar" <?> "This field is useless right now"
     } deriving (Generic)
 
 modifiers :: Modifiers
